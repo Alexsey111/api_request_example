62a374f783335095cf282cdb27f5b7fbfcbb6261
Alexsey111 <150332608+Alexsey111@users.noreply.github.com>
2024-05-13 10:15:55 +0700

    add all cod

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+import pprint
+
+params = {
+    'q': 'html'
+}
+
+response = requests.get('https://api.github.com/search/repositories', params=params)
+
+response_json = response.json()
+pprint.pprint(response_json)
+
+print(response.status_code)
+print(f'количество репозиториев с использованием html: {response_json["total_count"]}')
